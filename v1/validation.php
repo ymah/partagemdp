ab5fa1a0e082733245770fad9c84ba851b80d745
Mahieddine Yaker <myaker@runiso.com>
2014-08-22 17:10:05 +0200

    tout

--- v1/validation.php
+++ v1/validation.php
@@ -1,9 +1,30 @@
 <?php
+$_SESSION['index']++;
 session_save_path('temp');
 session_start();
-var_dump($_SESSION['captcha']['code']);
-var_dump($_POST['code']);
+$_SESSION['index']++;
 
-
-
+var_dump($_SESSION);
 ?>
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" />
+    <title>Partage Mot de passe</title>
+    <head>
+    <title>Inscription</title>
+    </head>
+    <body>
+
+
+<?php
+$_SESSION['index']++;
+print_r($_SESSION['captcha']['code']);
+echo "<br/>";
+$_SESSION['index']++;
+print_r($_POST['code']);
+print_r("<br/>".$_SESSION['index']);
+?>
+
+
+</body>
+</html>
